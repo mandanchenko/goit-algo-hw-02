719e61889c49187703670cc70d000faa543754b8
Andanchenko Maria <andanchenko.maria@gmail.com>
2023-12-22 10:34:32 +0200

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,18 +1,19 @@
 from collections import deque
 
-def is_palindrom(data:str):
-  data = data.replace(" ", "")
-  dq = deque(data.lower())
-  for i in range(len(data)//2):
-    a = dq.pop()
-    b = dq.popleft()
-    if a!= b:
-      return False
-  return True
+
+def is_pal(data):
+    data = data.replace(" ", "")
+    dq = deque(data.lower())
+    for i in range(len(data) // 2):
+        a = dq.pop()
+        b = dq.popleft()
+        if a != b:
+            return False
+    return True
 
 
-if __name__ == 'main':
-    print(is_palindrom('12nr12'))
-    print(is_palindrom('12n1n21'))
-    print(is_palindrom('Minim'))
-    print(is_palindrom('12nr12'))
+
+print(is_pal('12nr1 2'))
+print(is_pal('12n1n 21'))
+print(is_pal('Minim'))
+print(is_pal('12nr12'))
